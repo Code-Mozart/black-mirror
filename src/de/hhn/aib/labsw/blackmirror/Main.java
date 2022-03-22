0fea5f16d67b8eea458f7f70bf7a1b01933ce432
Markus Marewitz <marewitz@stud.hs-heilbronn.de>
2022-03-22 17:30:12 +0100

    Created basic framework and start-of-point for development

--- src/de/hhn/aib/labsw/blackmirror/Main.java
+++
@@ -1,7 +0,0 @@
-package de.hhn.aib.labsw.blackmirror;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Black Mirror");
-    }
-}
---
+++ src/de/hhn/aib/labsw/blackmirror/controller/Main.java
@@ -0,0 +1,32 @@
+package de.hhn.aib.labsw.blackmirror.controller;
+
+import de.hhn.aib.labsw.blackmirror.view.widgets.AbstractWidget;
+import de.hhn.aib.labsw.blackmirror.view.widgets.HelloWorldWidget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main class containing the entry point and controlling the program.
+ *
+ * @author Markus Marewitz
+ * @version 2022-03-22
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private List<AbstractWidget> widgets;
+
+    public Main() {
+        widgets = new ArrayList<>();
+
+        // @Team add your widgets here to test them -Markus
+        widgets.add(new
+                HelloWorldWidget()
+        );
+
+        widgets.forEach(w -> w.setVisible(true));
+    }
+}

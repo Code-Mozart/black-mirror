59c048766e8071e29c1ce94afb1316bea8d1779a
Markus Marewitz <marewitz@stud.hs-heilbronn.de>
2022-03-25 11:15:02 +0100

    mockup for calendar layout

---
+++ src/de/hhn/aib/labsw/blackmirror/view/widgets/CalendarWidget.java
@@ -0,0 +1,72 @@
+package de.hhn.aib.labsw.blackmirror.view.widgets;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+/**
+ * @author Markus Marewitz
+ * @version 2022-03-24
+ */
+public class CalendarWidget extends AbstractWidget {
+    private JLabel label;
+
+    public CalendarWidget() {
+        this.setSize(600, 400);
+        initComponents();
+    }
+
+    @Override
+    public void onNextSecond() {
+        ZonedDateTime now = ZonedDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofLocalizedDate(FormatStyle.FULL)
+                .withLocale(Locale.getDefault());
+        String dateString = formatter.format(now);
+        label.setText(dateString);
+    }
+
+    private void initComponents() {
+        JPanel panelMain = new JPanel(new BorderLayout());
+        panelMain.setBackground(Color.BLACK);
+
+        label = new JLabel("Time");
+        label.setForeground(Color.WHITE);
+
+        JPanel panelDays = new JPanel(new GridLayout(1, 7));
+        for (int i = 0; i < 7; i++) {
+            panelDays.add(new CalendarDayComponent());
+        }
+
+        panelMain.add(label, BorderLayout.NORTH);
+        panelMain.add(panelDays, BorderLayout.CENTER);
+        this.add(panelMain);
+    }
+
+    private static class CalendarDayComponent extends JPanel {
+        private final JLabel dateLabel;
+        private final JLabel dayLabel;
+
+        public CalendarDayComponent() {
+            this.setBackground(Color.BLACK);
+
+            this.setLayout(new BorderLayout());
+            this.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+
+            JPanel panelLabels = new JPanel(new GridLayout(2, 1));
+            panelLabels.setBackground(Color.BLACK);
+            this.add(panelLabels, BorderLayout.NORTH);
+
+            dateLabel = new JLabel("XX");
+            dateLabel.setForeground(Color.WHITE);
+            panelLabels.add(dateLabel);
+
+            dayLabel = new JLabel("Xx");
+            dayLabel.setForeground(Color.WHITE);
+            panelLabels.add(dayLabel);
+        }
+    }
+}

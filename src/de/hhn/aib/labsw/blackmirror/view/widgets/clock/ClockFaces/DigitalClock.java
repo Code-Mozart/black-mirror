e26f937eeb47d83432689d06c8be4c8fa0ce0109
Luis Gutzeit <lgutzeit@stud.hs-heilbronn.de>
2022-04-22 21:15:56 +0200

    implemented first version of flashing dot

--- src/de/hhn/aib/labsw/blackmirror/view/widgets/clock/ClockFaces/DigitalClock.java
+++ src/de/hhn/aib/labsw/blackmirror/view/widgets/clock/ClockFaces/DigitalClock.java
@@ -8,6 +8,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class DigitalClock implements ClockFace {
     @Override
@@ -24,7 +25,10 @@ public class DigitalClock implements ClockFace {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.setColor(Color.WHITE);
-            String time = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).format(LocalTime.now());
+            String time = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).format(LocalTime.now());
+            if(LocalTime.now().getSecond() % 2 == 0){
+                time = time.replace(":"," ");
+            }
             g.setFont(new Font("Calibri", Font.PLAIN,40));
             g.drawString(time,(getWidth()/2)-(getFontMetrics(getFont()).stringWidth(time)),(getHeight()/2)-(getFontMetrics(getFont()).getHeight()/2));
         }

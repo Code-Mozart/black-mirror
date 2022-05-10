65867ee7788fb1eeec6ed09dcbd245adf71d76d6
Luis Gutzeit <lgutzeit@stud.hs-heilbronn.de>
2022-05-10 09:55:24 +0200

    moved api from widget to controller

--- src/de/hhn/aib/labsw/blackmirror/view/widgets/AbstractWidget.java
+++ src/de/hhn/aib/labsw/blackmirror/view/widgets/AbstractWidget.java
@@ -15,8 +15,7 @@ import java.awt.*;
  * @author Markus Marewitz
  * @version 2022-03-24
  */
-public abstract class AbstractWidget extends JDialog implements TopicListener {
-    private final MirrorApi api = MirrorApiWebsockets.getInstance();
+public abstract class AbstractWidget extends JDialog {
 
     public enum Position {
         TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT, CENTER
@@ -42,35 +41,6 @@ public abstract class AbstractWidget extends JDialog implements TopicListener {
             case CENTER -> this.setLocation(screen.width / 2 - this.getWidth() / 2,
                     screen.height / 2 - this.getHeight() / 2);
         }
-    }
-
-    protected void subscribe(String topic, TopicListener listener) {
-        api.subscribe(topic, listener);
-    }
-    protected void unsubscribe(String topic, TopicListener listener) {
-        api.unsubscribe(topic, listener);
-    }
-    protected void publish(String topic, Object payload) {
-        api.publish(topic, payload);
-    }
-    protected void publish(String topic, JsonNode payload) {
-        api.publish(topic, payload);
-    }
-
-    @Override
-    public void dataReceived(String topic, JsonNode object) {
-    }
-
-    /**
-     * converts a JsonNode into the instance of a java record/dataclass
-     *
-     * @param node   the node to be converted
-     * @param tClass Class of which an instance should be created
-     * @return instance of the provided class filled with data from the json node
-     * @throws JsonProcessingException
-     */
-    protected <T> T nodeToObject(JsonNode node, Class<T> tClass) throws JsonProcessingException {
-        return api.getMapper().treeToValue(node, tClass);
     }
 }
 

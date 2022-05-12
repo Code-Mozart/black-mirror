f8348e2ab8626ee1f09dc7707186ff44b08033ef
Markus Marewitz <marewitz@stud.hs-heilbronn.de>
2022-05-12 14:48:44 +0200

    reorganized source files, fixed bugs

---
+++ src/main/java/de/hhn/aib/labsw/blackmirror/controller/Main.java
@@ -0,0 +1,52 @@
+package de.hhn.aib.labsw.blackmirror.controller;
+
+import de.hhn.aib.labsw.blackmirror.controller.API.MirrorApi;
+import de.hhn.aib.labsw.blackmirror.controller.API.websockets.MirrorApiWebsockets;
+import de.hhn.aib.labsw.blackmirror.view.widgets.clock.ClockFaceType;
+import de.hhn.aib.labsw.blackmirror.controller.widgets.*;
+import de.hhn.aib.labsw.blackmirror.view.widgets.AbstractWidget;
+
+import java.util.ArrayList;
+
+/**
+ * Main class containing the entry point and controlling the program.
+ *
+ * @author Markus Marewitz
+ * @author Niklas Binder
+ * @version 2022-05-11
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private final PageController pageController = new PageController();
+
+    public Main() {
+        MirrorApi server = MirrorApiWebsockets.getInstance();
+        server.init();
+
+        // @Team add your widgets here to test them -Markus
+        ArrayList<AbstractWidgetController> widgets = new ArrayList<>();
+        widgets.add(new ClockWidgetController(ClockFaceType.ANALOG));
+        widgets.add(new WeatherWidgetController());
+        widgets.add(new CalendarWidgetController());
+        widgets.add(new ReminderWidgetController());
+
+        int i = 0;
+        widgets.get(i++).getWidget().setPosition(AbstractWidget.Position.TOP_LEFT);
+        widgets.get(i++).getWidget().setPosition(AbstractWidget.Position.TOP_RIGHT);
+        widgets.get(i++).getWidget().setPosition(AbstractWidget.Position.BOTTOM_RIGHT);
+        widgets.get(i++).getWidget().setPosition(AbstractWidget.Position.BOTTOM_LEFT);
+
+        //@Team Use this method to add a new page, with a ArrayList of widgets -Niklas
+        pageController.addPage(widgets);
+
+        pageController.getCurrentPage().setWidgetsVisible(); //Sets all widgets on the default page visible.
+        new SecondsTimer(this::onNextSecond);
+    }
+
+    private void onNextSecond() {
+        pageController.getCurrentPage().onNextSecond();
+    }
+}
---
+++ src/main/java/de/hhn/aib/labsw/blackmirror/view/widgets/EmailNotificationWidget.java
@@ -0,0 +1,102 @@
+package de.hhn.aib.labsw.blackmirror.view.widgets;
+
+import de.hhn.aib.labsw.blackmirror.controller.widgets.EmailNotificationController;
+
+import javax.mail.*;
+//import jakarta.mail.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * This widget displays the number of unread mails in the users inbox
+ *
+ * @author Philipp Herda
+ * @version 2022-04-04
+ */
+public class EmailNotificationWidget extends AbstractWidget {
+
+    private final EmailNotificationController receiver;
+    private final ResourceBundle resources = ResourceBundle.getBundle("EmailNotificationWidget", Locale.getDefault());
+    private final JLabel textLabel;
+
+    private final String host;
+    private final String port;
+    private final String username;
+    private final String password;
+
+    private int counter = 0;
+
+    public EmailNotificationWidget() {
+        this.setSize(400, 300);
+        this.setLocation(500, 300);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+
+        panel.setLayout(new GridLayout(2, 1));
+
+        JLabel iconLabel = new JLabel("", SwingConstants.CENTER);
+        // icon author "Vecteezy.com"
+        ImageIcon mailIcon = new ImageIcon(getClass().getResource("/main/res/icons/mail.png"));
+        iconLabel.setIcon(mailIcon);
+
+        textLabel = new JLabel("", SwingConstants.CENTER);
+        textLabel.setForeground(Color.WHITE);
+        textLabel.setFont(new Font(textLabel.getFont().getFontName(), Font.BOLD, 16));
+
+        panel.add(iconLabel);
+        panel.add(textLabel);
+        this.add(panel);
+
+        receiver = new EmailNotificationController();
+
+        // todo: user data should later be obtained from db
+        // hotmail imap server: imap-mail.outlook.com
+        // web.de imap server: imap.web.de
+        // gmail.com imap server: imap.gmail.com
+        // Project Mail: "blackmirror.labswp@gmail.com", "labSWPproject" (error: app not a "secure app" for google)
+        host = "imap.web.de";
+        port = "993";
+        username = "blackmirror.labswp@web.de";
+        password = "labSWPproject";
+
+        try {
+            drawUnreadEmails();
+        } catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+
+    /**
+     * Displays the number of unread mails on the panel
+     *
+     * @throws MessagingException if connection failed
+     */
+    private void drawUnreadEmails() throws MessagingException {
+        try {
+            receiver.login(host, port, username, password);
+            textLabel.setText(receiver.checkForMails());
+        } catch (MessagingException me) {
+            me.printStackTrace();
+            textLabel.setText(resources.getString("failedToConnect"));
+        }
+    }
+
+//    /**
+//     * Checks every 30 seconds for new mail
+//     */
+//    @Override
+//    public void onNextSecond() {
+//        counter += 1;
+//        if (counter % 30 == 0) {
+//            try {
+//                drawUnreadEmails();
+//            } catch (MessagingException e) {
+//                e.printStackTrace();
+//            }
+//            counter = 0;
+//        }
+//    }
+}

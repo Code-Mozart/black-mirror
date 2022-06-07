829a8c87c3687fe860b17ec9f4ae14a87d72cd10
Philipp Herda <philippherda@hotmail.com>
2022-06-07 16:13:14 +0200

    added records for layout handling

---
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/model/ApiDataModels/LayoutData.java
@@ -0,0 +1,7 @@
+package de.hhn.aib.labsw.blackmirror.model.ApiDataModels;
+
+import java.util.ArrayList;
+
+public record LayoutData(ArrayList<WidgetData>[] widgets) {
+}
+
---
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/model/ApiDataModels/WidgetData.java
@@ -0,0 +1,4 @@
+package de.hhn.aib.labsw.blackmirror.model.ApiDataModels;
+
+public record WidgetData(String name, int x, int y) {
+}
